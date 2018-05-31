f843c9b9f40300ee698893fc4ce9b4f0ab9e1db0
Adminl <Adminl@EMER05>
2018-05-31 16:59:04 +0200

    ajout code algo

--- Algo3105AM/Program.cs
+++ Algo3105AM/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algo3105AM.classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +7,53 @@ using System.Threading.Tasks;
 
 namespace Algo3105AM
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-
-
+            
         }
 
-        private static int GererMenu(List<string> menu)
+        private static int GererMenu(List<MenuItem> menu)
         {
-            foreach (string ligne in menu)
+            foreach (MenuItem ligne in menu)
             {
                 Console.WriteLine(ligne);
             }
             int choix;
             choix = int.Parse(Console.ReadLine());
-            return choix;
 
+              Type test = typeof(int);
+
+
+            foreach (MenuItem item in menu)
+
+            {
+
+                if (choix == item.Numero)
+                return choix;
+            }
+            Console.WriteLine("Erreur de la saisie");
+            return -1;
+            
+
+            /*if (choix.GetType().Equals(test))
+            {
+                Console.WriteLine($"ok+{choix}");
+                return choix;
+            }
+            else { Console.WriteLine($"pas ok+{choix}"); return choix; }*/
 
         }
 
+
+
+        
+            
+
     }
+    
+    
 }
     
---
+++ Algo3105AM/classes/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo3105AM.classes
+{
+    class MenuItem
+    {
+        public int Numero { get; set; }
+        public string Libelle { get; set; }
+
+    }
+}
