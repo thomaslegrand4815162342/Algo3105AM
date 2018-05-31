8c82d54e3b239c748aea57230f9e55c08d114e80
Adminl <Adminl@EMER05>
2018-05-31 15:34:03 +0200

    ajout code algo

---
+++ Algo3105AM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo3105AM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        private static int GererMenu(List<string> menu)
+        {
+            foreach (string ligne in menu)
+            {
+                Console.WriteLine(ligne);
+            }
+            int choix;
+            choix = int.Parse(Console.ReadLine());
+            return choix;
+
+
+        }
+
+    }
+}
+    
